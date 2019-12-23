f5e560d9e7b54d43fd7434e09479fa6cdb56b7ae
djeriko <jerikosalivan@gmail.com>
2019-12-23 14:36:57 +0900

    created skeleton

--- lib/screens/bmi-calculator.dart
+++ lib/screens/bmi-calculator.dart
@@ -3,8 +3,31 @@ import 'package:flutter/material.dart';
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
